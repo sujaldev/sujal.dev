ceb7122af93ae636ad83d7637819451a4868a441
Sujal Singh <email.sujalsingh@gmail.com>
2025-07-11 10:35:49 +0530

    parametrize font subsetting script

--- src/fonts/subset.py
+++ src/fonts/subset.py
@@ -4,35 +4,33 @@ from pathlib import Path
 from fontTools.subset import main as subset
 
 SRC_DIR = Path(__file__).parent.parent.resolve()
-INPUT_FONT_DIR = SRC_DIR / "fonts"
-OUTPUT_FONT_DIR = SRC_DIR / "static/fonts/Libertinus"
 
 BASIC_LATIN = "0000-007F"
 LATIN_SUPPLEMENT = "0080-00FF"
 EVERYTHING_ELSE = "0100-10FFFF"
 
 
-def build_libertinus():
+def build_libertinus(input_font_dir: Path, output_font_dir: Path):
     # Clean output directory
-    if OUTPUT_FONT_DIR.exists():
-        shutil.rmtree(OUTPUT_FONT_DIR)
-    OUTPUT_FONT_DIR.mkdir(parents=True, exist_ok=True)
+    if output_font_dir.exists():
+        shutil.rmtree(output_font_dir)
+    output_font_dir.mkdir(parents=True, exist_ok=True)
 
     for font in ("Sans-Regular", "Sans-Italic", "Sans-Bold", "Mono-Regular"):
-        input_font = INPUT_FONT_DIR / f"Libertinus{font}.woff2"
+        input_font = input_font_dir / f"Libertinus{font}.woff2"
 
         for i, block in enumerate((BASIC_LATIN, LATIN_SUPPLEMENT, EVERYTHING_ELSE)):
             subset(args=[
                 str(input_font),
-                "--output-file=" + str(OUTPUT_FONT_DIR / f"{font}-{i + 1}.woff2"),
+                "--output-file=" + str(output_font_dir / f"{font}-{i + 1}.woff2"),
                 f"--unicodes={block}",
                 "--flavor=woff2",
                 "--layout-features='*'",
             ])
 
     # TODO: Figure out what to do regarding the math font.
-    shutil.copyfile(INPUT_FONT_DIR / "LibertinusMath-Regular.woff2", OUTPUT_FONT_DIR / "Math-Regular.woff2")
+    shutil.copyfile(input_font_dir / "LibertinusMath-Regular.woff2", output_font_dir / "Math-Regular.woff2")
 
 
 if __name__ == "__main__":
-    build_libertinus()
+    build_libertinus(SRC_DIR / "fonts/Libertinus", SRC_DIR / "static/fonts/Libertinus")
