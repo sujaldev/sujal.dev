9b49660074e3a6bb34b7f97b48473896d30ca55e
Sujal Singh <email.sujalsingh@gmail.com>
2025-07-15 20:29:27 +0530

    implemented cli interface

---
+++ src/cli.py
@@ -0,0 +1,61 @@
+import sys
+import argparse
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(
+        prog="ssg",
+        description="Static site generator for sujal.dev",
+    )
+
+    subparser = parser.add_subparsers(
+        title="command",
+        dest="command",
+    )
+
+    build_parser = subparser.add_parser(
+        name="build",
+        help="Build the site into build/ directory."
+    )
+    build_parser.add_argument(
+        "-m", "--minify", action="store_true", help="Enable minification."
+    )
+
+    live_parser = subparser.add_parser("live", help="Start a live server, only build pages on request.")
+    live_parser.add_argument(
+        "-b", "--bind", help="Bind to this address. (Default: 0.0.0.0)", dest="address", default="0.0.0.0"
+    )
+    live_parser.add_argument(
+        "-p", "--port", help="Bind to this port. (Default: 5000)", dest="port", default="5000"
+    )
+    live_parser.add_argument(
+        "-m", "--minify", action="store_true", help="Enable minification."
+    )
+
+    subset_fonts_parser = subparser.add_parser(
+        "subset-fonts", help="Generate font subsets and associated stylesheets."
+    )
+    subset_fonts_parser.add_argument(
+        "-c", "--css-only", action="store_true",
+        help="Do not regenerate font subsets, only font-face rules stylesheet template."
+    )
+
+    args = parser.parse_args(argv)
+
+    match args.command:
+        case "build":
+            from src.build import build
+            build(args.minify)
+        case "live":
+            from src.server import run as live
+            live(args.address, args.port, args.minify)
+        case "subset-fonts":
+            from src.fonts.subset import build as subset_fonts
+            subset_fonts(args.css_only)
+
+
+if __name__ == "__main__":
+    main()
--- src/server.py
+++ src/server.py
@@ -2,6 +2,8 @@ from src.build import load_config, make_jinja_env, build_home
 
 from flask import Flask
 
+MINIFIED = False
+
 app = Flask(__name__)
 env = make_jinja_env(live=True)
 
@@ -9,8 +11,15 @@ env = make_jinja_env(live=True)
 @app.route("/")
 def home():
     env.globals.update(load_config())
-    return build_home(env, minified=False, live=True)
+    return build_home(env, minified=MINIFIED, live=True)
+
+
+def run(host="0.0.0.0", port=5000, minify=False):
+    global MINIFIED
+    MINIFIED = minify
+
+    app.run(host=host, port=port, debug=True)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    run()
