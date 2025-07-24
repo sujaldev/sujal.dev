eee06402e6c6c468ceb5a2d5dccf88fd8eab3786
Sujal Singh <email.sujalsingh@gmail.com>
2025-07-24 13:12:37 +0530

    drop live_config decorator
    this is in preparation for a file watch based approach for reloading config and other files as well

--- ssg/server.py
+++ ssg/server.py
@@ -1,5 +1,3 @@
-import functools
-
 from ssg.build import *
 from ssg.constants import *
 
@@ -11,23 +9,12 @@ app = Flask(__name__)
 env = make_jinja_env(live=True)
 
 
-def live_config(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        env.globals.update(load_config())
-        return func(*args, **kwargs)
-
-    return wrapper
-
-
 @app.route("/")
-@live_config
 def home():
     return build_home(env, minified=MINIFIED, live=True)
 
 
 @app.route("/blog")
-@live_config
 def blog():
     return build_blog(env, minified=MINIFIED, live=True)
 
