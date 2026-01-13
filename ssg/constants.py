56fdc49e7ddbb1262f5316e2b964b0eafef6b32c
Sujal Singh <email.sujalsingh@gmail.com>
2026-01-13 16:59:41 +0530

    init ssg v2

--- ssg/constants.py
+++ ssg/constants.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 
 __all__ = [
-    "PROJECT_ROOT", "CONTENT_DIR", "SRC_DIR", "BUILD_DIR", "HASH_CACHE_FILE",
+    "ROOT", "BUILD_DIR", "DIST_DIR",
 ]
 
-PROJECT_ROOT = Path(__file__).parent.parent.resolve()
-CONTENT_DIR = PROJECT_ROOT / "content"
-SRC_DIR = PROJECT_ROOT / "ssg"
-BUILD_DIR = PROJECT_ROOT / "build"
-HASH_CACHE_FILE = PROJECT_ROOT / ".cache/hashes.csv"
+ROOT = Path(__file__).parent.parent.resolve() / "src"
+BUILD_DIR = ROOT / "build"
+DIST_DIR = BUILD_DIR / "dist"
--- ssg/include/README.md
+++
@@ -1,9 +0,0 @@
-# Include
-
-Files in this directory are meant to be included inline in jinja templates using:
-
-```jinja
-{% include_raw("file_path") %}
-```
-
-The purpose for this is that these files are too small to warrant a separate request.
